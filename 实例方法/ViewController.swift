0b7aa6cae8d859a6ed30aaf8210632c04ea210c8
LiuPW <liupw@LiuPWtekiMacBook-Air.local>
2014-06-18 22:06:27 +0800

    Initial Commit

---
+++ 实例方法/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  实例方法
+//
+//  Created by LiuPW on 14-6-18.
+//  Copyright (c) 2014年 LiuPW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
